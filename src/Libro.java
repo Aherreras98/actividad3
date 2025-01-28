51830f77877c3ee2392261c6b4608370fb30789e
Adrián  Herrera <aherrerasosa98@gmail.com>
2025-01-28 02:38:34 +0100

    Creacion de clase Libro con atributos y getters.

---
+++ src/Libro.java
@@ -0,0 +1,21 @@
+package GestorBiblioteca;
+
+class Libro {
+    private String titulo;
+    private String autor;
+    private String categoria;
+    private boolean prestado;
+
+    public Libro(String titulo, String autor, String categoria) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.categoria = categoria;
+        this.prestado = false;
+    }
+
+    public String getTitulo() { return titulo; }
+    public String getAutor() { return autor; }
+    public String getCategoria() { return categoria; }
+    public boolean isPrestado() { return prestado; }
+    public void setPrestado(boolean prestado) { this.prestado = prestado; }
+}
