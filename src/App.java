78238e10cc6f9fadac8484b15927e878f3f674c0
Javier Villar <javi.villar.jimenez@gmail.com>
2025-01-28 19:41:43 +0100

    He creado la clase usuario para poder validar la entrada de los usuarios

--- src/App.java
+++ src/App.java
@@ -1,5 +1,14 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+package GestorBiblioteca;
+class Usuario {
+private String nombre;
+private String contrasena;
+private boolean esAdmin;
+public Usuario(String nombre, String contrasena, boolean esAdmin) {
+this.nombre = nombre;
+this.contrasena = contrasena;
+this.esAdmin = esAdmin;
+}
+public boolean validarContrasena(String contrasena) {
+return this.contrasena.equals(contrasena);
+}
 }
