8370cc58391d23de591f095cffb44167b6295d3f
Adrián  Herrera <aherrerasosa98@gmail.com>
2025-01-28 15:03:50 +0100

    Package y esqueleto gestorlibro con array de libros

---
+++ src/GestorBiblioteca/GestorLibro.java
@@ -0,0 +1,14 @@
+package GestorBiblioteca;
+
+class GestorLibro {
+    private Libro[] libros = new Libro[0];
+
+    public void agregarLibro(Libro libro) {
+        // Pendiente
+    }
+
+    public boolean eliminarLibro(String titulo) {
+        // Pendiente 
+        return false;
+    }
+}
