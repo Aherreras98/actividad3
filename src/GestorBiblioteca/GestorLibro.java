ef3ab1e292e1ef0f90604631436ff4a299684c66
Adrián  Herrera <aherrerasosa98@gmail.com>
2025-01-29 13:44:14 +0100

    Agregar y eliminar libros

--- src/GestorBiblioteca/GestorLibro.java
+++ src/GestorBiblioteca/GestorLibro.java
@@ -1,14 +1,26 @@
 package GestorBiblioteca;
 
+import java.util.Arrays;
+
 class GestorLibro {
     private Libro[] libros = new Libro[0];
 
-    public void agregarLibro(Libro libro) {
-        // Pendiente
-    }
+public void agregarLibro(Libro libro) {
+    Libro[] nuevoArray = Arrays.copyOf(libros, libros.length + 1);
+    nuevoArray[libros.length] = libro;
+    libros = nuevoArray;
+}
 
-    public boolean eliminarLibro(String titulo) {
-        // Pendiente 
-        return false;
+public boolean eliminarLibro(String titulo) {
+    for (int i = 0; i < libros.length; i++) {
+        if (libros[i].getTitulo().equalsIgnoreCase(titulo)) {
+            Libro[] nuevoArray = new Libro[libros.length - 1];
+            System.arraycopy(libros, 0, nuevoArray, 0, i);
+            System.arraycopy(libros, i + 1, nuevoArray, i, nuevoArray.length - i);
+            libros = nuevoArray;
+            return true;
+        }
     }
+    return false;
+}
 }
